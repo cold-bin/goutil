18d52e4553fc2d1e85a5bee3a2cc0cc4e12d156b
阿冰 <3398341353@qq.com>
2022-10-23 22:53:56 +0800

    feat(web): add encapsulation for HTTP GET & Post

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module goutil
 
 go 1.17
 
-require github.com/gin-gonic/gin v1.8.1
+require (
+	github.com/gin-gonic/gin v1.8.1
+	github.com/juju/ratelimit v1.0.2
+)
 
 require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
---
+++ web/http.go
@@ -0,0 +1,67 @@
+// @author cold bin
+// @date 2022/10/22
+
+package web
+
+import (
+	"errors"
+	"goutil/internal/_err"
+	"io"
+	"net/http"
+	"net/url"
+	"sync"
+)
+
+// 池化
+var reqPool = sync.Pool{New: func() interface{} {
+	return http.DefaultClient
+}}
+
+// HttpGet 封装http get方法请求数据（get方法默认没有请求体）
+func HttpGet(url string) (data []byte, e _err.Err) {
+	var c *http.Client
+	var ok bool
+	if c, ok = reqPool.Get().(*http.Client); !ok {
+		return nil, _err.WrapErr(errors.New("req pool is wrong"))
+	}
+
+	response, err := c.Get(url)
+	if err != nil {
+		return nil, _err.WrapErr(err)
+	}
+
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, _err.WrapErr(err)
+	}
+
+	return bytes, _err.Err{}
+}
+
+func HttpPost(url string, contentType string, body io.Reader) (data []byte, e _err.Err) {
+	response, err := http.Post(url, contentType, body)
+	if err != nil {
+		return nil, _err.WrapErr(err)
+	}
+
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, _err.WrapErr(err)
+	}
+
+	return bytes, _err.Err{}
+}
+
+func HttpPostForm(url string, datas url.Values) (data []byte, e _err.Err) {
+	response, err := http.PostForm(url, datas)
+	if err != nil {
+		return nil, _err.WrapErr(err)
+	}
+
+	bytes, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, _err.WrapErr(err)
+	}
+
+	return bytes, _err.Err{}
+}
